98c1d65122db53d093ae1d37f5c65a1a60312ec1
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-05-08 11:51:03 +0200

    LandPage with features

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,11 +1,32 @@
 import Banner from "./Internal-components/Banner"
+import StorageOutlinedIcon from '@mui/icons-material/StorageOutlined';
+import FastForwardOutlinedIcon from '@mui/icons-material/FastForwardOutlined';
+import SsidChartOutlinedIcon from '@mui/icons-material/SsidChartOutlined';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
 
 const Features = () => {
+
+    /**
+     * databaseText, arrowsFast, statsLine, stopImage
+     */
+
+    const databaseText = 'Ningun dato personal ni ningun trackeo'
+    const arrowFast = 'Rapidez ante todo, sin esperas innecesarias'
+    const statsLine = 'Conoce las estadisticas que generan tus enlaces'
+    const stopImage = 'Sin ninguna publicidad ni popup ni banner ajeno a pipo-short'
+
+    const data = [
+        {name:'0 datos Personales', image:StorageOutlinedIcon, text:databaseText },
+        {name:'A tu velocidad', image:FastForwardOutlinedIcon, text:arrowFast},
+        {name:'Stats para tus links', image:SsidChartOutlinedIcon, text:statsLine},
+        {name:'Sin publicidad', image:CancelOutlinedIcon, text:stopImage}
+    ]
+
 
     return (
         <section id="features--main">
             
-            <Banner />
+            <Banner data={data}/>
 
         </section>
     )
--- src/components/Features/Internal-components/Banner.jsx
+++ src/components/Features/Internal-components/Banner.jsx
@@ -2,7 +2,15 @@ const Banner = (props) => {
 
     return(
         <section className="banner--main">
-
+            {props.data.map((e,i) => {
+                return <section className={`banner--card-body banner--card-${i+1}`} key={i}>
+                    <div className="banner--card-image">
+                        <e.image fontSize="large"/>
+                    </div>
+                    <h4 className="banner--card-title">{e.name}</h4>
+                    <p className="banner--card-text">{e.text}</p>
+                </section>
+            })}
         </section>
     )
 }
