aa16977a8f2205517fb9108c718e01e19380abbc
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-05-12 17:34:43 +0200

    preparando primera conexion con back: short url anonima

--- src/components/InputLink/InputLink.jsx
+++ src/components/InputLink/InputLink.jsx
@@ -1,9 +1,27 @@
+import { useState } from "react"
+import axios from "axios"
+
 const InputLink = () => {
+    const [url, shortUrl] = useState('');
+
+    const submit = (e) => {
+        if(e.key === 'Enter')
+            encryptUrl(url)
+    }
+
+    const encryptUrl = async(myUrl) => {
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}url/shorturl`,
+        { data: {url:myUrl, user:'anon'}})
+
+        console.log(response)
+
+    }
 
     return(
         <div className="inputlink--main">
             <label className="inputlink--label">Your link:</label>
-            <input type="text" className="inputlink--input" placeholder="input your link"/>
+            <input type="text" onKeyDown={submit} onChange={e => shortUrl(e)} className="inputlink--input" placeholder="input your link"/>
+            <button>Acortar</button>
         </div>
     )
 }
