b56e8e5e8a16552da6eb6820e5b1c9553375d99c
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-05-13 18:19:40 +0200

    agregado button a input link y funcionalidad para llamar a la api

--- src/components/InputLink/InputLink.jsx
+++ src/components/InputLink/InputLink.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 import axios from "axios"
 
 const InputLink = () => {
     const [url, shortUrl] = useState('');
+    const textInput = useRef();
 
     const submit = (e) => {
         if(e.key === 'Enter')
@@ -10,18 +11,18 @@ const InputLink = () => {
     }
 
     const encryptUrl = async(myUrl) => {
-        const response = await axios.post(`${process.env.REACT_APP_API_URL}url/shorturl`,
-        { data: {url:myUrl, user:'anon'}})
+        /* const response = await axios.post(`${process.env.REACT_APP_API_URL}url/shorturl`,
+        { data: {url:myUrl, user:'anon'}}) */
 
-        console.log(response)
+        console.log(myUrl)
 
     }
 
     return(
         <div className="inputlink--main">
             <label className="inputlink--label">Your link:</label>
-            <input type="text" onKeyDown={submit} onChange={e => shortUrl(e)} className="inputlink--input" placeholder="input your link"/>
-            <button>Acortar</button>
+            <input type="text" ref={textInput} onKeyDown={submit} onChange={e => shortUrl(e.target.value)} className="inputlink--input" placeholder="input your link"/>
+            <button className="inputlink--button" onClick={e => encryptUrl(textInput.current.value)}>Acortar</button>
         </div>
     )
 }
