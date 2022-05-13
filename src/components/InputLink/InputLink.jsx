b36831742a8b22ec1073e8b2aaea6aad326f5bd2
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-05-13 19:00:14 +0200

    preparado para acortar url

--- src/components/InputLink/InputLink.jsx
+++ src/components/InputLink/InputLink.jsx
@@ -2,7 +2,7 @@ import { useRef, useState } from "react"
 import axios from "axios"
 
 const InputLink = () => {
-    const [url, shortUrl] = useState('');
+    const [url, setUrl] = useState('');
     const textInput = useRef();
 
     const submit = (e) => {
@@ -11,17 +11,26 @@ const InputLink = () => {
     }
 
     const encryptUrl = async(myUrl) => {
-        /* const response = await axios.post(`${process.env.REACT_APP_API_URL}url/shorturl`,
-        { data: {url:myUrl, user:'anon'}}) */
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}url/shorturl`,
+        { data: {url:myUrl, user:'anon'}})
 
-        console.log(myUrl)
+        setUrl(response.data)
 
+        textInput.current.value = response.data
+
+        if(navigator.clipboard){
+            await navigator.clipboard.writeText(response.data)
+        }else{
+            console.log('Pipo lo siente, tu navegador no es compatible con el copiado automatico')
+        }
+
+        
     }
 
     return(
         <div className="inputlink--main">
             <label className="inputlink--label">Your link:</label>
-            <input type="text" ref={textInput} onKeyDown={submit} onChange={e => shortUrl(e.target.value)} className="inputlink--input" placeholder="input your link"/>
+            <input type="text" ref={textInput} onKeyDown={submit} onChange={e => setUrl(e.target.value)} className="inputlink--input" placeholder="input your link"/>
             <button className="inputlink--button" onClick={e => encryptUrl(textInput.current.value)}>Acortar</button>
         </div>
     )
