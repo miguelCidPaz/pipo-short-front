7a4ef6208a47be98d3d4b88cefd4430d0dc6d1a0
miguelCidPaz <miguel.cid.paz@gmail.com>
2022-05-15 09:02:59 +0200

    finiquitado register

--- src/components/Forms/Internal-components/Register.jsx
+++ src/components/Forms/Internal-components/Register.jsx
@@ -2,6 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import {validation} from './tools'
 import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import { useIsFocusVisible } from "@mui/material";
 
 const Register = () => {
     const [viewPass, setViewPass] = useState(false)
@@ -9,7 +10,7 @@ const Register = () => {
     const [pass, setPass] = useState('');
     const [validateUser, setValidateUser] = useState(false)
     const [validatePass, setValidatePass] = useState(false)
-    const [retypePass, setRetypePass] = useState(false)
+    const [validateRetypePass, setValidateRetypePass] = useState(false)
     const inputUser = useRef();
     const inputPass = useRef();
 
@@ -22,32 +23,42 @@ const Register = () => {
     },[pass])
 
     const verifyPass = (text) => {
-        setRetypePass(text === pass)
+        setValidateRetypePass(text === pass)
+    }
+
+    const validateForm = () => {
+        if(validateUser && validatePass && validateRetypePass){
+            if(username.length > 0 && pass.length > 0)
+                if(validation(username) && validation(pass))
+                    console.log(`${username} & ${pass}`)
+        }
     }
 
     return (
         <div className="form--section">
-            <input type="text" ref={inputUser} className='form--input' placeholder='Write your name' onChange={e => setUsername(e.target.value)} />
+            <input type="text" ref={inputUser} className={validateUser ? 'form--input' : 'form--input-invalidate'} placeholder='Write your name' onChange={e => setUsername(e.target.value)} />
             <div className='form--group'>
                 {!viewPass ?
                     <>
                         <div className='form--slot'>
-                            <input type="password" placeholder='Write your password' ref={inputPass} className='form--input form--pass' onChange={e => setPass(e.target.value)} />
+                            <input type="password" placeholder='Write your password' ref={inputPass} className={validatePass ? 'form--input form--pass' : 'form--input-invalidate form--pass-invalidate'} onChange={e => setPass(e.target.value)} />
                             <button className='form--button-view' onClick={e => setViewPass(!viewPass)}><VisibilityIcon className='form--icon' /></button>
                         </div>
-                        <input type="password" onChange={e => verifyPass(e.target.value)} className='form--input' placeholder='Retype your password' />
+                        <input type="password" onChange={e => verifyPass(e.target.value)} className={validateRetypePass ? 'form--input' : 'form--input-invalidate'} placeholder='Retype your password' />
                     </> :
                     <>
                         <div className='form--slot'>
-                            <input type='text' placeholder='Write your password' ref={inputPass} className='form--input form--pass' onChange={e => setPass(e.target.value)}/>
+                            <input type='text' placeholder='Write your password' ref={inputPass} className={validatePass ? 'form--input form--pass' : 'form--input-invalidate form--pass-invalidate'} onChange={e => setPass(e.target.value)}/>
                             <button className='form--button-view-inverse' onClick={e => setViewPass(!viewPass)}><VisibilityOffIcon className='form--icon' /></button>
                         </div>
-                        <input type="text" onChange={e => verifyPass(e.target.value)} className='form--input' placeholder='Retype your password' />
+                        <input type="text" onChange={e => verifyPass(e.target.value)} className={validateRetypePass ? 'form--input' : 'form--input-invalidate'} placeholder='Retype your password' />
 
                     </>
                 }
             </div>
-            <button className='form--submit' >Register</button>
+            {validateUser && validatePass && validateRetypePass ?
+            <button onClick={e => validateForm()} className='form--submit' >Register</button> :
+            <button disabled className='form--submit form--submit-disabled'>Register</button>}
         </div>
     )
 }
